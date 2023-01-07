fd7ab0cf1ead7b85c018667ff6edcd7a9298720f
Mitrofanov Mikhail <z.pack@mail.ru>
2023-01-07 22:17:54 +0300

    4 times I start. It seemed to turn out a little. Sorry, that main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,7 +15,7 @@ import util.CreatorIsland;
 import java.io.File;
 import java.io.IOException;
 
-public class Main {
+public class Main {//
     public static void main(String[] args) {
 
         //Constant загружаем из файла
